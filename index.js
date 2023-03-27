45e73c67c4041a069c854d95e03e38530609b0ff
Nada Marei <eng.nada.marey.hassan@gmail.com>
2023-03-27 23:17:49 +0200

    initial commit

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+// use .env for secret keys
 dotenv.config();
 
+// connected to MongoDB
 mongoose.connect(
-    process.env.MONGO_URL
+    process.env.MONGO_URL 
   );
 
 app.listen(3000, ()=>{
