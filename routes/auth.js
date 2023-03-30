bf17393ff63cb9b99a163c610e1e63749ba3a3f2
Nada Marei <eng.nada.marey.hassan@gmail.com>
2023-03-30 20:24:06 +0200

    update

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ router.get("/register", async (req, res) => {
     const user = await new User({
         username:"nada",
         email:"nada@gamil.com",
-        password:123456
+        password:123456,
     })
     await user.save();
     res.send("OK");
